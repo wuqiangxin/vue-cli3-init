2fe92e960daad4d30447b9f2848832130c2d9522
青柄紫鞘 <2498052298@qq.com>
2020-08-07 22:40:29 +0800

    vue cli3 项目初始化完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Main from '../views/main.vue'
+import Hello from '../views/Holle.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +20,21 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: Main
+  },
+  {
+    path: '/hello',
+    name: 'Hello',
+    component: Hello
   }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-button type="primary" icon="el-icon-edit"></el-button>
+    <el-button type="primary" icon="el-icon-share"></el-button>
+    <el-button type="primary" icon="el-icon-delete"></el-button>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <el-button type="primary">
+      上传
+      <i class="el-icon-upload el-icon--right"></i>
+    </el-button>
+
+    <h1>login 页面</h1>
+
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+  </div>
+</template>
+
